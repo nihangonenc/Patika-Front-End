ce09558b4b5ce6ae8a47a71d1a46cee474487560
nihangonenc <nnurgonenc@gmail.com>
2024-06-14 22:45:36 +0300

    BillGates added

---
+++ Week-8/BillGates/src/components/List/List.jsx
@@ -0,0 +1,16 @@
+
+import ListInfo from "./ListInfo";
+
+
+function List() {
+  return (
+    <div>
+      <h1 style={{textAlign : "center", padding : "30px" }}>Receipt</h1>
+      <ul>
+        <ListInfo/>
+      </ul>
+    </div>
+  );
+}
+
+export default List;
